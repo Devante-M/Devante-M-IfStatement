e6fe5d32d3bc279cf4e33c2b31da3687bbc0d8e4
Devante McFadden <applejuicedrinker21@gmail.com>
2023-03-08 16:42:32 +0000

    Lab completed

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -19,7 +19,15 @@ public class Lab {
      * @param y second number to be compared.
      * @return x if bool is true. if bool is false, return y.
      */
+
+
     public int ifExample(boolean bool, int x, int y){
-        return 0;
-    }
+    if (bool){
+        return x;
+    }else{
+        return y;
+    } 
+        }
+
 }
+
